0585600f58eac09bce6eeb4099e10afac2828776
yangm <yangm@email.com>
2021-04-22 18:50:35 +0800

    绑定openid到user用户信息上

---
+++ shop/apps/oauth/serializers.py
@@ -0,0 +1,60 @@
+from django_redis import get_redis_connection
+from rest_framework import serializers
+from .models import OauthQQUser
+from users.models import UserInfo
+from .utils import check_user_token_openid
+class QQauthUserSerializer(serializers.ModelSerializer):
+    # 需要反序列化 字段 username password mobile sms_code access_token
+    sms_code = serializers.CharField(label='验证码')
+    mobile = serializers.RegexField(label='手机号',regex=r'^1[3-9]\d{9}$')
+    password = serializers.CharField(label='密码',min_length=8,max_length=20)
+    access_token = serializers.CharField(label='openid',max_length=128)
+
+    def validate(self, attrs):
+        # 对openid进行及解密 获取到openid
+        access_token = attrs['access_token']
+        openid = check_user_token_openid(access_token)
+        if not openid:
+            raise serializers.ValidationError('无效的openid')
+
+        # 获取到短信验证码 从redis数据库中取得并进行校验
+        redis_conn = get_redis_connection('verify_codes')
+        mobile = attrs['mobile']
+        real_code = redis_conn.get('sms_%s' %mobile)
+        if attrs['sms_code'] != real_code.decode():
+            raise serializers.ValidationError('验证码错误！')
+        if real_code == None:
+            raise serializers.ValidationError('无效的验证码！')
+        try:
+            user = UserInfo.objects.get(mobile = mobile)
+        except UserInfo.DoesNotExist:
+            pass
+        else:
+            password = attrs['password']
+            if not user.check_password(password):
+                raise serializers.ValidationError('密码错误')
+            attrs['user'] = user
+
+        return attrs
+
+        # 通过手机号寻找用户 如果有 则将openid 与用户进行绑定
+        # 如果没有 则需要新建用户 再将openid与用户进行绑定
+    def create(self, validated_data):
+        user = validated_data.get('user')
+
+        if not user:
+            # 用户不存在 则新建用户
+            user = UserInfo.objects.Create_user(
+                username = validated_data['mobile'],
+                password = validated_data['password'],
+                mobile = validated_data['mobile'],
+            )
+        OauthQQUser.objects.create(
+            openid=validated_data['open_id'],
+            user = user
+        )
+        return user
+
+
+
+
--- shop/apps/oauth/views.py
+++ shop/apps/oauth/views.py
@@ -1,16 +1,101 @@
 from django.shortcuts import render
 from QQLoginTool.QQtool import OAuthQQ
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from .models import OauthQQUser
 from rest_framework.views import APIView
 from django.conf import settings
-
+from rest_framework_jwt.settings import api_settings
+from . import utils
+from . import serializers
+# Create your views here.
+logger = settings.LOGGING()
 
 class QQAuthURLView(APIView):
-
+    '''获取qq认证路径'''
     def get(self,request):
-
+        # next 表示从哪个界面进入到登录界面  登陆之后再返回到之前的页面
         next = request.query_params.get('next')
-        pass
+        if not next:
+            next = '/'
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,client_secret=settings.QQ_CLIENT_SECRET
+                        ,redirect_uri=settings.QQ_REDIRECT_URI,state=next)
+        # 构建qq认证的url
+        login_url = oauth.get_qq_url()
+
+        return Response({'login_url':login_url})
+
+
+class QQAuthUserView(APIView):
+    ''' 创建Oauth对象 调用函数 获取token值  再通过token 获取到openID
+
+    '''
+    def get(self,request):
+        code = request.query_params.get('code')
+        if not code:
+            return Response({'message':'code值为空'},status=status.HTTP_400_BAD_REQUEST)
+        # 得到code值 使用加密方式
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID, client_secret=settings.QQ_CLIENT_SECRET
+                        , redirect_uri=settings.QQ_REDIRECT_URI)
+        try:
+            access_t = oauth.get_access_token(code=code)
+            # 每个QQ的openid是固定的
+            openid = oauth.get_open_id(access_token=access_t)
+        except Exception as e:
+            logger.info(e)
+            return  Response({'message':'QQ服务器异常'},status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        # 使用openID查询该qq是否在商城绑定过用户
+        # 如果没有绑定 则将openID加密后返回前端暂时存储一下 以便后面绑定用户时使用
+        # 如果绑定了 则手动生成JWT_token 返回到前端
+
+        try:
+            oauthUser_model = OauthQQUser.objects.get(openid=openid)
+        except OauthQQUser.DoesNotExist:
+            access_token = utils.generate_user_openid_token(openid)
+            return  Response({'access_token':access_token})
+        else:
+            # 使用jwt生成token
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+            # 从QQ模型中获取关联的user
+            user = oauthUser_model.user
+
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
+
+            return Response({
+                'user_id':user.id,
+                'username' :user.username,
+                'token':token
+            })
+
+    def post(self,request):
+        ''' 使用 openid 绑定用户'''
+        serializer = serializers.QQauthUserSerializer(data=request.data)
+        # 校验数据
+        serializer.is_valid()
+        # 保存校验结果 并接收数据
+        user = serializer.save()
+        # 使用jwt生成token
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        return Response({
+            'user_id': user.id,
+            'username': user.username,
+            'token': token
+        })
+
+
+
+
+
+
+
+
 
 
 
